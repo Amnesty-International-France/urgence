ea121de30513e2dac79be81af454b644223676b3
ThieryMichel <thiery@marmelab.com>
2018-04-18 11:04:00 +0200

    augment timeout for e2e test

--- front/e2e/app.test.js
+++ front/e2e/app.test.js
@@ -1,10 +1,14 @@
 import { By } from 'selenium-webdriver';
 import driver from './driver';
 
-describe('app', () => {
-    it('should display home', async () => {
-        await driver.get('http://app-e2e:3000');
-        const text = await driver.findElement(By.tagName('body')).getText();
-        expect(text).toBe('Home');
-    });
-});
+describe(
+    'app',
+    () => {
+        it('should display home', async () => {
+            await driver.get('http://app-e2e:3000');
+            const text = await driver.findElement(By.tagName('body')).getText();
+            expect(text).toBe('Home');
+        });
+    },
+    20000,
+);
