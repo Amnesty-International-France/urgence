773da6830c1aa7ade55a1dab0d5c085c3efa2afa
ThieryMichel <thiery@marmelab.com>
2018-04-20 13:45:52 +0200

    add Apollo to fetch UA

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { View } from 'react-primitives';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import { Router, Route } from './gateway/ReactRouter';
 import generateUrl from './services/generateUrl';
 import Home from './Home';
 import UA from './UA';
 
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+});
+
 class App extends Component {
     render() {
         return (
-            <Router>
-                <View>
-                    <Route exact path={generateUrl('home')} component={Home} />
-                    <Route path={generateUrl('ua')} component={UA} />
-                </View>
-            </Router>
+            <ApolloProvider client={client}>
+                <Router>
+                    <View>
+                        <Route
+                            exact
+                            path={generateUrl('home')}
+                            component={Home}
+                        />
+                        <Route path={generateUrl('ua')} component={UA} />
+                    </View>
+                </Router>
+            </ApolloProvider>
         );
     }
 }
--- front/src/UA.js
+++ front/src/UA.js
@@ -1,32 +1,52 @@
 import React from 'react';
-
 import glamorous from 'glamorous-primitives';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Text, View } from 'react-primitives';
 
-import Carousel from './Carousel';
-
-const Item = glamorous.view({
-    backgroundColor: 'darkorange',
-    padding: 50,
-    textAlign: 'center',
-    width: '100%',
-});
-
-const ItemText = glamorous.text({
-    color: 'white',
-});
+import Title from './themes/Title';
 
 const UA = () => (
-    <Carousel>
-        <Item>
-            <ItemText>1</ItemText>
-        </Item>
-        <Item>
-            <ItemText>2</ItemText>
-        </Item>
-        <Item>
-            <ItemText>3</ItemText>
-        </Item>
-    </Carousel>
+    <Query
+        query={gql`
+            query {
+                UrgentAction(id: "7ed78132-4b66-4320-a9f2-dd9c1a6552c2") {
+                    id
+                    title
+                }
+            }
+        `}
+    >
+        {({ loading, error, data }) => {
+            if (loading) {
+                return (
+                    <View>
+                        <Text>Loading...</Text>
+                    </View>
+                );
+            }
+            if (error) {
+                return <Text>Error: {error}</Text>;
+            }
+
+            if (!data.UrgentAction) {
+                return <Title>Not found</Title>;
+            }
+
+            return <Title>{data.UrgentAction.title}</Title>;
+        }}
+        {/* <Carousel>
+            <Item>
+                <ItemText>1</ItemText>
+            </Item>
+            <Item>
+                <ItemText>2</ItemText>
+            </Item>
+            <Item>
+                <ItemText>3</ItemText>
+            </Item>
+        </Carousel> */}
+    </Query>
 );
 
 export default UA;
