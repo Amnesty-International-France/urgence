b760b251ecad6ab44b9c53169040645ebde5d489
ThieryMichel <thiery@marmelab.com>
2018-04-20 16:23:36 +0200

    add GraphqlQuery helper component

--- front/src/UA.js
+++ front/src/UA.js
@@ -1,47 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Text, View } from 'react-primitives';
 import { withRouter } from 'react-router';
 
 import Title from './themes/Title';
+import GraphqlQuery from './helperComponents/GraphqlQuery';
 
-const UA = ({
-    match: {
-        params: { id },
-    },
-}) => (
-    <Query
-        query={gql`
-            query urgentAction($id: ID!) {
-                UrgentAction(id: $id) {
-                    id
-                    title
-                }
-            }
-        `}
-        variables={{ id }}
-    >
-        {({ loading, error, data }) => {
-            if (loading) {
-                return (
-                    <View>
-                        <Text>Loading...</Text>
-                    </View>
-                );
-            }
-            if (error) {
-                return <Text>Error: {error.message}</Text>;
-            }
+export const renderUA = data => <Title>{data.UrgentAction.title}</Title>;
 
-            if (!data.UrgentAction) {
-                return <Title>Not found</Title>;
-            }
+const UAquery = gql`
+    query urgentAction($id: ID!) {
+        UrgentAction(id: $id) {
+            id
+            title
+        }
+    }
+`;
 
-            return <Title>{data.UrgentAction.title}</Title>;
-        }}
-    </Query>
+const UA = ({ match: { params } }) => (
+    <GraphqlQuery query={UAquery} variables={params} render={renderUA} />
 );
 
 UA.propTypes = {
---
+++ front/src/helperComponents/GraphqlQuery.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import { Text, View } from 'react-primitives';
+
+import Title from '../themes/Title';
+
+const GraphqlQuery = ({ query, variables, render }) => (
+    <Query query={query} variables={variables}>
+        {({ loading, error, data }) => {
+            if (loading) {
+                return (
+                    <View>
+                        <Text>Loading...</Text>
+                    </View>
+                );
+            }
+            if (error) {
+                return <Text>Error: {error.message}</Text>;
+            }
+
+            if (!data.UrgentAction) {
+                return <Title>Not found</Title>;
+            }
+
+            return render(data);
+        }}
+    </Query>
+);
+
+GraphqlQuery.propTypes = {
+    query: PropTypes.object.isRequired,
+    variables: PropTypes.object.isRequired,
+    render: PropTypes.func.isRequired,
+};
+
+export default GraphqlQuery;
