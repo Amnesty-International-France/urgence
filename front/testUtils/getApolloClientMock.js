8ebef53ea43b04ee20d2dd2f056bd72484b98dc1
ThieryMichel <thiery@marmelab.com>
2018-04-23 14:51:46 +0200

    fix test after rebase

--- front/testUtils/getApolloClientMock.js
+++ front/testUtils/getApolloClientMock.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { SchemaLink } from 'apollo-link-schema';
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import typeDefs from '../../api/src/graphql/typeDefs';
+import { typeDefs } from '../../api/src/graphql/typeDefs';
 
 const defaultMocks = {
     UrgentAction: () => null,
