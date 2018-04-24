e7adec21a57956b8a5e1926f25ea7685ca7239ed
ThieryMichel <thiery@marmelab.com>
2018-04-24 11:12:17 +0200

    add omitDeep helper

---
+++ api/src/utils/omitDeep.js
@@ -0,0 +1,21 @@
+const omitDeep = (keys, data) => {
+    if (Array.isArray(data)) {
+        return data.map(data => omitDeep(keys, data));
+    }
+    if (Object.prototype.toString.call(data) === '[object Object]') {
+        return Object.keys(data).reduce((acc, key) => {
+            if (keys.indexOf(key) !== -1) {
+                return acc;
+            }
+
+            return {
+                ...acc,
+                [key]: omitDeep(keys, data[key]),
+            };
+        }, {});
+    }
+    return data;
+
+}
+
+export default omitDeep;
