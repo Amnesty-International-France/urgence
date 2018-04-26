1cf92fdb0262a3bb601607efe718c80f75307d58
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2018-04-26 18:03:05 +0200

    Review design of story steps

---
+++ admin/src/urgentActions/MediumInput.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import get from 'lodash.get';
+import {
+    TextInput,
+    ImageInput,
+    ImageField
+} from 'react-admin';
+import { withStyles } from 'material-ui/styles';
+
+const styles = {
+    root: {
+        display: 'flex',
+    },
+    titleWrapper: {
+        flex: '1 0 0',
+    },
+    imageWrapper: {
+        flex: '1 0 0',
+    },
+};
+
+export const MediumInput = ({ classes, source }) => (
+    <div className={classes.root}>
+        <label>medium</label>
+        <div className={classes.titleWrapper}>
+            <TextInput source="title" label="Medium title" /><br />
+        </div>
+        <div className={classes.imageWrapper}>
+            <ImageInput source="src" label="File">
+                <ImageField source="src" title="Preview" />
+            </ImageInput>
+        </div>
+    </div>
+);
+
+MediumInput.propTypes = {
+    classes: PropTypes.object,
+    source: PropTypes.string.isRequired,
+};
+
+export default withStyles(styles)(MediumInput);
