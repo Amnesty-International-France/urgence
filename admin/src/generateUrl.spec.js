4e02c8e8ef185c5a3e41cbf0ca04359c3d61f8f9
ThieryMichel <thiery@marmelab.com>
2018-04-26 15:23:01 +0200

    fix test

--- admin/src/generateUrl.spec.js
+++ admin/src/generateUrl.spec.js
@@ -4,7 +4,7 @@ describe('generateUrl', () => {
     describe('generateAppUrl', () => {
         it('should render correct urgent action route', () => {
             const url = generateAppUrl('urgentAction', { id: '123456-abcdef' });
-            expect(url).toBe('http://localhost:3000/UA/123456-abcdef');
+            expect(url).toBe('http://localhost:3000/#/UA/123456-abcdef');
         });
     });
 })
