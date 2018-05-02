31ba250e4bdc7cba08d1a93e933f156a922a3d79
ThieryMichel <thiery@marmelab.com>
2018-05-02 12:40:42 +0200

    add test on MessageStep

---
+++ front/src/urgentActions/message/MessageStep.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { MessageStep } from './MessageStep';
+
+describe('<MessageStep />', () => {
+    const defaultProps = {
+        content: 'Abdolfatah Soltani is a Human Rights Iranian lawyer.',
+    };
+
+    it('should display step content as HTML', () => {
+        const props = {
+            ...defaultProps,
+            content: '<p>Hello world!</p>',
+        };
+
+        const wrapper = shallow(<MessageStep {...props} />);
+        const richText = wrapper.find('RichText');
+        expect(richText.prop('html')).toBe('<p>Hello world!</p>');
+    });
+});
