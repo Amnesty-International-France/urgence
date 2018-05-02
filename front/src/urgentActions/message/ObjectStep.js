48d9d1e62624270311bcbd12332dbc0df9cd91f6
ThieryMichel <thiery@marmelab.com>
2018-05-02 11:28:33 +0200

    add state to ObjectState to store object input value

--- front/src/urgentActions/message/ObjectStep.js
+++ front/src/urgentActions/message/ObjectStep.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
 
 import RichText from '../../themes/RichText';
 import CarouselSlide from '../../themes/CarouselSlide';
 
-export const ObjectStep = ({ objectIndication, className }) => (
-    <CarouselSlide className={className}>
-        <p>
-            Parce que les messages uniques ont plus d&#39;impact, nous vous
-            invitons à personnaliser son sujet.
-        </p>
-        <textarea rows="5" />
-        <RichText html={objectIndication} />
-    </CarouselSlide>
-);
+export class ObjectStep extends Component {
+    state = {
+        object: '',
+    };
+
+    changeObject = e => {
+        this.setState({ object: e.target.value });
+    };
+
+    render() {
+        const { objectIndication, className } = this.props;
+        const { object } = this.state;
+
+        return (
+            <CarouselSlide className={className}>
+                <p>
+                    Parce que les messages uniques ont plus d&#39;impact, nous
+                    vous invitons à personnaliser son sujet.
+                </p>
+                <textarea
+                    rows="5"
+                    value={object}
+                    onChange={this.changeObject}
+                />
+                <RichText html={objectIndication} />
+            </CarouselSlide>
+        );
+    }
+}
 
 ObjectStep.propTypes = {
     className: PropTypes.string,
