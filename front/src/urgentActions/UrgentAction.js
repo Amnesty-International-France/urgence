d673fa2b4c8e4dd19b3dfb76a3adccc0e14cd60c
ThieryMichel <thiery@marmelab.com>
2018-05-03 15:41:43 +0200

    use saved color on front

--- front/src/urgentActions/UrgentAction.js
+++ front/src/urgentActions/UrgentAction.js
@@ -18,6 +18,7 @@ const query = gql`
                 displayOptions {
                     mediumPosition
                     backgroundColor
+                    color
                 }
                 medium {
                     src
