5506a92f3cd611aa88b556feaac840b37c5fb47d
ThieryMichel <thiery@marmelab.com>
2018-05-03 10:51:18 +0200

    add test on validateMail

--- admin/src/urgentActions/getUrgentActionFormFragment.spec.js
+++ admin/src/urgentActions/getUrgentActionFormFragment.spec.js
@@ -1,4 +1,4 @@
-import { validateMedium } from './getUrgentActionFormFragment';
+import { validateMedium, validateMail } from './getUrgentActionFormFragment';
 
 describe('validateMedium', () => {
     const record = {
@@ -32,5 +32,24 @@ describe('validateMedium', () => {
         expect(
             validateMedium('osef', record, null, 'story[3].medium.src')
         ).toBe('You need to specify both src and title for medium or none of them');
+    });
+});
+
+describe('validateMail', () => {
+    it('should return null if mail is valid', () => {
+        expect(validateMail('mail@mail')).toBe(null);
+        expect(validateMail('mail@gmail.com')).toBe(null);
+    });
+
+    it('should return null if comma separated list of mails are valids', () => {
+        expect(validateMail('mail@mail,other@mail,third@web.com')).toBe(null);
+    });
+
+    it('should return error message if mail is invalid', () => {
+        expect(validateMail('notamail')).toBe('Must contain only mail separated by ","');
+    });
+
+    it('should return error message if one of the mail is invalid', () => {
+        expect(validateMail('mail@mail,notamail,valid@gmail.com')).toBe('Must contain only mail separated by ","');
     });
 });
