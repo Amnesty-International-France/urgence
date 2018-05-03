1e007c869cde3414212dba16c676998229aac6e1
ThieryMichel <thiery@marmelab.com>
2018-05-03 10:38:56 +0200

    add recipient to urgentAction table

---
+++ api/migrations/20180503083446-urgent_action-recipient.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, cb) {
+  const sql = `
+      ALTER TABLE urgent_action ADD COLUMN recipient JSON;
+  `;
+
+  db.runSql(sql, cb);
+};
+
+exports.down = function(db, cb) {
+  const sql = `
+      ALTER TABLE urgent_action DROP COLUMN recipient;
+  `;
+
+  db.runSql(sql, cb);
+};
+
+exports._meta = {
+  "version": 1
+};
