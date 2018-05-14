33652226ac44a3321ad5356ed9c344654e5a976b
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2018-05-14 16:19:55 +0200

    Add PDF letter preview in admin

--- admin/src/generateUrl.spec.js
+++ admin/src/generateUrl.spec.js
@@ -6,5 +6,10 @@ describe('generateUrl', () => {
             const url = generateAppUrl('urgentAction', { id: '123456-abcdef' });
             expect(url).toBe('http://localhost:3000/#/UA/123456-abcdef');
         });
+
+        it('should return correct urgent action letter preview URL', () => {
+            const url = generateAppUrl('urgentActionLetter', { id: '87d1d7c4-0ecc-48c4-bcce-64bab69e0a61' });
+            expect(url).toBe('http://localhost:4000/urgent-actions/87d1d7c4-0ecc-48c4-bcce-64bab69e0a61.pdf?subject=Custom%20subject&signature=John%20Doe');
+        });
     });
 })
