0f2750368546955505cb0b40907dabe2ad10079c
ThieryMichel <thiery@marmelab.com>
2018-05-14 12:15:36 +0200

    add medium src migration

---
+++ api/migrations/20180514095321-full-image-url.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const config = require('../../config');
+
+exports.up = function(db, cb) {
+    return db.runSql(`
+    UPDATE urgent_action SET story=(REPLACE(story::TEXT, 'src":"', 'src":"${config.uploadUrl}/'))::JSON
+    `, cb);
+};
+
+exports.down = function(db) {
+  return db.runSql(`
+  UPDATE urgent_action SET story=(REPLACE(story::TEXT, 'src":"${config.uploadUrl}/', 'src":"'))::JSON
+  `, cb);
+};
+
+exports._meta = {
+  "version": 1
+};
