c85305f0e629007d3cf4f2df45ad67da6b063394
ThieryMichel <thiery@marmelab.com>
2018-05-24 11:41:29 +0200

    add test on generateUrl

--- front/src/services/generateUrl.spec.js
+++ front/src/services/generateUrl.spec.js
@@ -13,10 +13,36 @@ describe('generateUrl', () => {
         expect(generateUrl('ua', { id: 'id_value' })).toBe('/ua/id_value');
     });
 
+    it('should generate url for ua/id/story', () => {
+        expect(generateUrl('story', { id: 'id_value' })).toBe('/ua/id_value/story/0');
+    });
+
+    it('should generate url for ua/id/story step', () => {
+        expect(generateUrl('story', { id: 'id_value', page: 5 })).toBe('/ua/id_value/story/5');
+    });
+
+    it('should generate url for ua/id/act', () => {
+        expect(generateUrl('act', { id: 'id_value' })).toBe('/ua/id_value/act');
+    });
+
+    it('should generate url for ua/id/message', () => {
+        expect(generateUrl('message', { id: 'id_value' })).toBe('/ua/id_value/message');
+    });
+
+    it('should generate url for ua/id/object', () => {
+        expect(generateUrl('object', { id: 'id_value' })).toBe('/ua/id_value/object');
+    });
+
+    it('should generate url for ua/id/signature', () => {
+        expect(generateUrl('signature', { id: 'id_value' })).toBe('/ua/id_value/signature');
+    });
+
+    it('should generate url for ua/id/thanks', () => {
+        expect(generateUrl('thanks', { id: 'id_value' })).toBe('/ua/id_value/thanks');
+    });
+
     it('should throw error if receiving unknow route', () => {
-        expect(() => generateUrl()).toThrow(
-            'Unknown route undefined passed to generateUrl',
-        );
+        expect(() => generateUrl()).toThrow('Unknown route undefined passed to generateUrl');
 
         expect(() => generateUrl('bad_route')).toThrow(
             'Unknown route bad_route passed to generateUrl',
