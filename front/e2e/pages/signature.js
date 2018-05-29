94a41739614894af435952537734d795d718c902
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2018-05-29 09:10:05 +0200

    Fix tests

--- front/e2e/pages/signature.js
+++ front/e2e/pages/signature.js
@@ -3,7 +3,7 @@ import { By, until } from 'selenium-webdriver';
 export default driver => {
     const elements = {
         validateButton: By.css('.action a'),
-        textarea: By.css('textarea'),
+        input: By.css('input'),
         sendMailButton: By.css('.action a'),
         container: By.className('signature'),
     };
@@ -13,7 +13,7 @@ export default driver => {
             await driver.navigate().to(`http://front:3000/#/ua/${id}/signature`);
             await this.isLoaded();
         },
-        enterText: async value => driver.findElement(elements.textarea).sendKeys(value),
+        enterText: async value => driver.findElement(elements.input).sendKeys(value),
         getMailTo: async () => driver.findElement(elements.sendMailButton).getAttribute('href'),
         isButtonDisabled: async () => {
             const className = await driver
