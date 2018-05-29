3d9953f5959aa307f8fbd4c2304a06a6c9b9c8db
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2018-05-29 14:16:32 +0200

    Fix preview link in admin

--- admin/src/generateUrl.js
+++ admin/src/generateUrl.js
@@ -1,12 +1,24 @@
+import { stringify } from 'qs';
+
 export const generateAppUrl = (route, params) => {
     switch (route) {
         case 'urgentAction':
             return `${process.env.REACT_APP_FRONT_BASE_URL}/#/UA/${params.id}`;
 
         case 'urgentActionLetter':
-            return `${process.env.REACT_APP_API_URL}/urgent-actions/${params.id}.pdf?subject=Custom%20subject&signature=John%20Doe`;
+            return `${process.env.REACT_APP_API_URL}/urgent-actions/${params.id}.pdf?${stringify({
+                subject: 'Custom Subject',
+                signature: 'John Doe',
+                address: `
+                    Amnesty International
+                    Le Chaumontois
+                    72-76, boulevard de la Villette
+                    75019 Paris
+                    France
+                `,
+            })}`;
 
         default:
             throw new Error(`Unknown route: ${route}`);
     }
-}
+};
