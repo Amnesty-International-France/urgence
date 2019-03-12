15e4529d6ce31f6cfa122eb13d903912a9f5c6da
Luwangel <adrien.amoros@gmail.com>
2019-03-12 12:40:24 +0100

    Design Act screen

--- front/src/urgentActions/Act.js
+++ front/src/urgentActions/Act.js
@@ -4,15 +4,60 @@ import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
 
 import RichText from '../themes/RichText';
-import { yellow, white } from '../themes/colors';
+import { yellow, white, black } from '../themes/colors';
 import { withBlackLogo } from '../themes/ThemeContext';
 import { LinkType } from '../propTypes';
 import Link from './Link';
 
+const styles = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: '100%',
+    padding: '1rem',
+    color: white,
+    backgroundColor: yellow,
+    fontFamily: 'Amnesty Trade Gothic',
+    '@media (min-width: 1024px)': {
+        padding: '10vh 10vw',
+        '& .link': {
+            textAlign: 'center',
+        },
+    },
+    '& h1': {
+        textTransform: 'uppercase',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        margin: '2rem 0.5rem',
+        width: 'calc(100% - 0.5rem)',
+    },
+    '& span': {
+        color: white,
+        backgroundColor: black,
+        padding: '0.7rem 0',
+        boxShadow: `0.5rem 0 0 ${black}, -0.5rem 0 0 ${black}`,
+    },
+    '& .rich-text': {
+        color: black,
+        fontSize: '1rem',
+    },
+    '& .actions': {
+        margin: '2rem 0',
+        '& a': {
+            display: 'inline-block',
+            color: yellow,
+            backgroundColor: black,
+            height: 40,
+        },
+    },
+};
+
 export const Act = ({ callToAction, className, action }) => (
     <div className={className}>
         <div>
-            <h1>{get(callToAction, 'title')}</h1>
+            <h1>
+                <span>{get(callToAction, 'title')}</span>
+            </h1>
             <RichText html={get(callToAction, 'message')} />
         </div>
         <div className="actions">
@@ -32,37 +77,4 @@ Act.propTypes = {
     className: PropTypes.string.isRequired,
 };
 
-export default glamorous(withBlackLogo(Act))({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    backgroundColor: yellow,
-    color: white,
-    height: '100%',
-    '@media (min-width: 1024px)': {
-        padding: '10vh 10vw',
-        '& .link': {
-            textAlign: 'center',
-        },
-    },
-    '& h1': {
-        fontFamily: 'Amnesty Trade Gothic Condensed',
-        padding: '2rem 3rem',
-        textAlign: 'center',
-        fontSize: '56px',
-        fontWeight: 'bold',
-    },
-    '& .rich-text': {
-        margin: '22px 31px',
-        textAlign: 'center',
-        fontSize: '28px',
-        lineHeight: '33px',
-        fontWeight: 'bold',
-    },
-    '& .actions': {
-        textAlign: 'center',
-    },
-    '& a': {
-        margin: '0 1rem 1rem 1rem',
-    },
-});
+export default glamorous(withBlackLogo(Act))(styles);
