c95e5b7fe02e9c8f273763da05493c6bc94f25e2
Luwangel <adrien.amoros@gmail.com>
2019-03-13 10:23:20 +0100

    Refact ThanksInput

--- admin/src/urgentActions/ThanksInput.js
+++ admin/src/urgentActions/ThanksInput.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-import { addField, FormDataConsumer, TextInput, LongTextInput } from 'react-admin';
+import { addField, FormDataConsumer, TextInput } from 'react-admin';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 
-import { root, preview } from './styles';
-import { merge } from './defaultFormData';
+import { root, transitionScreenPreview } from './styles';
 import { get as getScreenIndex, CONTINUE, THANKS } from './screenIndex';
-import FrontPreview from './FrontPreview';
+import RichTextInput from '../form/RichTextInput';
+import FrontPreview, { noop } from './FrontPreview';
 
 import Thanks from '../../../front/src/urgentActions/Thanks';
 import Link from '../../../front/src/themes/Link';
@@ -19,14 +19,7 @@ import LinkInput from './LinkInput';
 const styles = {
     ...root,
     preview: {
-        ...preview,
-        // the rules below override desktop media queries so that the preview is forced to appear like on mobile
-        '& > div': {
-            padding: '0 !important',
-            '& a': {
-                alignSelf: 'auto',
-            },
-        },
+        ...transitionScreenPreview,
     },
 };
 
@@ -41,27 +34,25 @@ export const ThanksInput = ({ classes, source, withLink, final }) => (
                     <Card className={classes.card}>
                         <CardContent className={classes.content}>
                             <div className={classes.formContainer}>
-                                <div>
-                                    <TextInput
-                                        source={`${source}.title`}
-                                        label="Title"
-                                        defaultValue="Merci de votre soutien !"
-                                    />
-                                    <LongTextInput
-                                        source={`${source}.text`}
-                                        label="Text"
-                                        multiline
-                                    />
-                                    {withLink && <LinkInput source={`${source}.link`} />}
-                                </div>
+                                <TextInput
+                                    source={`${source}.title`}
+                                    label="Title"
+                                    defaultValue="Merci de votre soutien !"
+                                />
+                                <RichTextInput source={`${source}.text`} label="Text" />
+                                <TextInput source={`${source}.button`} label="Button" />
+                                {!final && withLink && <LinkInput source={`${source}.link`} />}
                             </div>
                         </CardContent>
                     </Card>
                     <FrontPreview className={classes.preview}>
                         <Thanks
-                            title={merge(formData)[source].title}
-                            text={merge(formData)[source].text}
-                            actions={() => (final ? null : <Link to={'#'} label="Continuer" />)}
+                            data={formData[source]}
+                            actions={() =>
+                                final ? null : (
+                                    <Link to="#" label={formData[source].button} onClick={noop} />
+                                )
+                            }
                         />
                     </FrontPreview>
                 </Fragment>
@@ -71,14 +62,13 @@ export const ThanksInput = ({ classes, source, withLink, final }) => (
 );
 
 ThanksInput.propTypes = {
-    source: PropTypes.string,
     classes: PropTypes.object,
+    source: PropTypes.string,
     withLink: PropTypes.bool,
     final: PropTypes.bool,
 };
 
 ThanksInput.defaultProps = {
-    source: '',
     final: false,
     withLink: false,
 };
