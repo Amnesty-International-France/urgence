445ffea8d324bd1ce83b84e28320137570a494cf
Luwangel <adrien.amoros@gmail.com>
2019-03-15 10:03:00 +0100

    Fix AppLogo tests

--- front/src/themes/AppLogo.spec.js
+++ front/src/themes/AppLogo.spec.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { AppLogo } from './AppLogo';
+import AmnestyLogo from '../icons/AmnestyLogo';
 
 describe('<AppLogo />', () => {
     const defaultProps = {
         context: {},
     };
 
-    it('should display Amnesty Candle in correct color (taken from context)', () => {
+    it('should display AmnestyLogo in correct color (taken from context)', () => {
         const props = {
             ...defaultProps,
             context: {
-                logoColor: 'blue',
+                color: 'blue',
+                backgroundColor: 'red',
             },
         };
 
         const wrapper = shallow(<AppLogo {...props} />);
-        expect(wrapper.find('AmnestyCandle').prop('fill')).toBe('blue');
+        const logo = wrapper.find(AmnestyLogo);
+        expect(logo.length).toEqual(1);
+        expect(logo.prop('fill0')).toEqual('red');
+        expect(logo.prop('fill1')).toEqual('blue');
     });
 });
