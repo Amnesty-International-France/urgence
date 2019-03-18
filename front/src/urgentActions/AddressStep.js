7451cd8426e312392938582d7a0ad8539157de58
JulienM <julienm@marmelab.com>
2019-03-18 10:44:51 +0100

    Design

--- front/src/urgentActions/AddressStep.js
+++ front/src/urgentActions/AddressStep.js
@@ -6,15 +6,14 @@ import classnames from 'classnames';
 
 import { withSessionData } from '../SessionDataContext';
 import { withYellowLogo } from '../themes/ThemeContext';
-import TextArea from '../themes/TextArea';
+import Input from '../themes/Input';
 const styles = {
     fontFamily: 'Amnesty Trade Gothic LT',
     fontSize: '16px',
     display: 'flex',
     flexDirection: 'column',
     backgroundColor: 'white',
-    height: '100%',
-    padding: '105px 2rem 53px',
+    padding: '100px 20px 20px 20px',
     justifyContent: 'space-between',
     '& textarea': {
         width: '100%',
@@ -25,9 +24,16 @@ const styles = {
     },
     '& .action': {
         marginTop: '53px',
+        '& > a': {
+            marginBottom: 20,
+        },
     },
     '& p': {
-        margin: '20px 0px',
+        marginBottom: 20,
+    },
+    '& .lastParagraph': {
+        marginTop: 20,
+        marginBottom: 0,
     },
     '& li': {
         margin: '16px 0px',
@@ -44,13 +50,44 @@ const styles = {
         paddingLeft: 35,
         borderLeft: 'solid black 2px',
     },
+    '& .cityZone': {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    '& .oneTiers': {
+        width: '33%',
+    },
+    '& .twoTiers': {
+        width: '62%',
+    },
 };
 
 export class AddressStep extends Component {
-    setAddress = event => this.props.setAddress(event.target.value);
+    setAddressMain = event => this.props.setAddressMain(event.target.value);
+    setAddressMore = event => this.props.setAddressMore(event.target.value);
+    setPostalCode = event => this.props.setPostalCode(event.target.value);
+    setCity = event => this.props.setCity(event.target.value);
+    setCountry = event => this.props.setCountry(event.target.value);
+    setEmail = event => this.props.setEmail(event.target.value);
+
+    isEnabled = () => {
+        const { addressMain, postalCode, city, country, email } = this.props;
+        return !!addressMain && !!postalCode && !!city && !!country && !!email;
+    };
 
     render() {
-        const { action, className, address } = this.props;
+        const {
+            action,
+            className,
+            addressMain,
+            addressMore,
+            postalCode,
+            city,
+            country,
+            email,
+        } = this.props;
+
         return (
             <div className={classnames('address', className)}>
                 <div>
@@ -75,28 +112,70 @@ export class AddressStep extends Component {
                         Afin de personnaliser votre courrier, merci de renseigner votre adresse
                         postale.
                     </p>
-                    <TextArea
-                        rows="6"
-                        value={address}
-                        onChange={this.setAddress}
-                        placeholder="Votre adresse postale"
+                    <Input
+                        className="addressMain"
+                        value={addressMain}
+                        onChange={this.setAddressMain}
+                        label="Adresse"
                     />
-                    <p>
+                    <Input
+                        className="addressMore"
+                        value={addressMore}
+                        onChange={this.setAddressMore}
+                        label="Complément d'adresse"
+                    />
+                    <div className="cityZone">
+                        <Input
+                            className={classnames('oneTiers', 'postalCode')}
+                            value={postalCode}
+                            onChange={this.setPostalCode}
+                            label="Code postal"
+                        />
+                        <Input
+                            className={classnames('twoTiers', 'city')}
+                            value={city}
+                            onChange={this.setCity}
+                            label="Ville"
+                        />
+                    </div>
+                    <Input
+                        className={classnames('twoTiers', 'country')}
+                        value={country}
+                        onChange={this.setCountry}
+                        label="Pays"
+                    />
+                    <p className="lastParagraph">
                         Pour recevoir le courrier finalisé,{' '}
                         <strong className="importantText">
                             veuillez renseigner votre adresse e-mail.
                         </strong>
                     </p>
+                    <Input
+                        className="email"
+                        value={email}
+                        onChange={this.setEmail}
+                        label="E-mail"
+                    />
                 </div>
-                <div className="action">{action(!address)}</div>
+                <div className="action">{action(this.isEnabled())}</div>
             </div>
         );
     }
 }
 
 AddressStep.propTypes = {
-    address: PropTypes.string.isRequired,
-    setAddress: PropTypes.func.isRequired,
+    addressMain: PropTypes.string.isRequired,
+    addressMore: PropTypes.string.isRequired,
+    postalCode: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    setAddressMain: PropTypes.func.isRequired,
+    setAddressMore: PropTypes.func.isRequired,
+    setPostalCode: PropTypes.func.isRequired,
+    setCity: PropTypes.func.isRequired,
+    setCountry: PropTypes.func.isRequired,
+    setEmail: PropTypes.func.isRequired,
     className: PropTypes.string,
     action: PropTypes.func,
 };
