d713a6d304cdc5142d3ea0cf746386ea082f8265
JulienM <julienm@marmelab.com>
2019-03-19 14:20:24 +0100

    Fix transparence border issue on iphone

--- front/src/urgentActions/message/Message.js
+++ front/src/urgentActions/message/Message.js
@@ -49,7 +49,7 @@ const styles = {
     },
     '& .letter': {
         border: 'solid 1px',
-        borderColor: 'rgb(0, 0, 0, 0.20)',
+        borderColor: 'rgba(0, 0, 0, 0.20)',
         boxShadow:
             '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
         color: black,
