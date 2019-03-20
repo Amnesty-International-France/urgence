da842d82f3c6ef7d094d077b9c56ae5da309af6b
Luwangel <adrien.amoros@gmail.com>
2019-03-20 16:42:54 +0100

    Fix sorting in admin

--- admin/src/urgentActions/UrgentActionList.js
+++ admin/src/urgentActions/UrgentActionList.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import {
-    Datagrid,
-    EditButton,
-    List,
-    Responsive,
-    TextField,
-    DateField,
-} from 'react-admin';
+import { Datagrid, EditButton, List, Responsive, TextField, DateField } from 'react-admin';
 
 import PreviewLink from './PreviewLink';
 import PreviewLetter from './PreviewLetter';
 
-const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+const dateFormat = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+};
 
-export default (props) => (
-    <List
-        {...props}
-        title={'Urgent Actions'}
-        sort={{ field: 'date', order: 'DESC' }}
-        perPage={25}
-    >
+const sort = { field: 'last_edition_date', order: 'DESC' };
+
+export default props => (
+    <List {...props} title={'Urgent Actions'} sort={sort} perPage={25}>
         <Responsive
             medium={
                 <Datagrid>
