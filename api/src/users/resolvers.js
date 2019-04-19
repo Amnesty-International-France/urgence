70647c0c57fe402a96c120031cbc87b3f68076f9
Luwangel <adrien.amoros@gmail.com>
2019-04-19 11:51:36 +0200

    Export default resolvers

--- api/src/users/resolvers.js
+++ api/src/users/resolvers.js
@@ -6,21 +6,27 @@ import config from '../../../config';
 export const createToken = (user, now = new Date()) => {
     const expiration = addHours(now, config.admin.authentication.sessionDuration).toISOString();
 
-    return jwt.sign({
-        ...user,
-        expiration,
-    }, config.admin.authentication.jwtSecret);
+    return jwt.sign(
+        {
+            ...user,
+            expiration,
+        },
+        config.admin.authentication.jwtSecret,
+    );
 };
 
 export const login = (_, { username, password }) => {
-    if (username !== config.admin.authentication.username || password !== config.admin.authentication.password) {
+    if (
+        username !== config.admin.authentication.username ||
+        password !== config.admin.authentication.password
+    ) {
         throw new Error('Invalid credentials.');
     }
 
     return { token: createToken({ role: 'admin' }) };
 };
 
-export const UsersResolvers = {
+export default {
     Query: {
         login,
     },
