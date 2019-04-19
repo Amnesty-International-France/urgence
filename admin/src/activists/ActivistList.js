dcc79183bfbdc3ecfb5ff069f575a749abd37f25
Luwangel <adrien.amoros@gmail.com>
2019-04-19 11:48:12 +0200

    Create an ActivistList

---
+++ admin/src/activists/ActivistList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Datagrid, List, Responsive, TextField, DateField } from 'react-admin';
+
+const dateFormat = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+};
+
+const sort = { field: 'created_on', order: 'DESC' };
+
+export default props => (
+    <List {...props} title="Activists" sort={sort} perPage={25}>
+        <Responsive
+            medium={
+                <Datagrid>
+                    <TextField source="id" />
+                    <DateField
+                        source="created_on"
+                        label="Created On"
+                        options={dateFormat}
+                        showTime
+                    />
+                    <TextField source="email" />
+                    <TextField source="firstname" />
+                    <TextField source="lastname" />
+                </Datagrid>
+            }
+        />
+    </List>
+);
