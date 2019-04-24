3a531d1e950145d2dcb58f4558be3e64c90d2ac1
Luwangel <adrien.amoros@gmail.com>
2019-04-24 16:34:42 +0200

    Setup icons

--- admin/src/activists/index.js
+++ admin/src/activists/index.js
@@ -1,6 +1,8 @@
 import ActivistList from './ActivistList';
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 
 export default {
     name: 'Activists',
+    icon: RecordVoiceOverIcon,
     list: ActivistList,
 };
