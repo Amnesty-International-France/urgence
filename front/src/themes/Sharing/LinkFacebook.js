ba16a2893d8f8d7bc967c9741c093c58a756b76f
JulienM <julienm@marmelab.com>
2019-04-26 11:48:27 +0200

    Replace facebook by messenger

--- front/src/themes/Sharing/LinkFacebook.js
+++ front/src/themes/Sharing/LinkFacebook.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ShareLink from './ShareLink';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
 
 export const LinkFacebook = ({ url, action }) => (
     <ShareLink
-        href={`https://facebook.com/sharer/sharer.php?u=${url}`}
+        href={`fb-messenger://share/?link=${encodeURIComponent(url)}`}
         target="facebook"
-        title="Partage facebook"
-        icon={faFacebook}
-        text="Partager sur facebook"
+        title="Partage Messenger"
+        icon={faFacebookMessenger}
+        text="Partager sur Messenger"
         action={action}
-        customClass="fb-share-button"
-        customScript={
-            <script
-                async
-                defer
-                crossOrigin="anonymous"
-                src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.2"
-            />
-        }
     />
 );
 
