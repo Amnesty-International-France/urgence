c6484a081affdaa9040fe3a7c9d776649a257713
Luwangel <adrien.amoros@gmail.com>
2019-09-03 17:08:54 +0200

    Fix Content RichText in SettingsCreate

--- admin/src/settings/SettingsCreate.js
+++ admin/src/settings/SettingsCreate.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Create, SimpleForm, SelectInput, required } from 'react-admin';
-import RichTextInput from '../form/RichTextInput';
+import RichTextInput from 'ra-input-rich-text';
 
 import types from './types';
+
+const toolbar = [[{ size: ['small', 'normal', 'large'] }, 'bold', 'italic', 'underline', 'link']];
 
 export default props => (
     <Create title="Create New Setting" {...props}>
         <SimpleForm>
-            <SelectInput source="type" choices={types} validate={required()} />
-            <RichTextInput source="content" validate={required()} />
+            <SelectInput source="type" label="Type" choices={types} validate={required()} />
+            <RichTextInput
+                source="content"
+                label="Content *"
+                toolbar={toolbar}
+                validate={required()}
+            />
         </SimpleForm>
     </Create>
 );
