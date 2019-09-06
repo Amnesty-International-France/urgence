f559957186de2e33d4815f60304fa2097e738958
Luwangel <adrien.amoros@gmail.com>
2019-09-06 18:53:04 +0200

    Remove activists from api

--- api/src/graphql/resolvers.js
+++ api/src/graphql/resolvers.js
@@ -1,6 +1,5 @@
-import ActivistsResolvers from '../activists/resolvers';
 import UrgentActionsResolver from '../urgentActions/resolvers';
 import UsersResolvers from '../users/resolvers';
 import SettingsResolvers from '../settings/resolvers';
 
-export default [ActivistsResolvers, UrgentActionsResolver, UsersResolvers, SettingsResolvers];
+export default [UrgentActionsResolver, UsersResolvers, SettingsResolvers];
