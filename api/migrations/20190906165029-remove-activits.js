f5050676df5ff53904b36149a771fbb829aa15b7
Luwangel <adrien.amoros@gmail.com>
2019-09-06 18:52:16 +0200

    Create a migration to remove activists

---
+++ api/migrations/20190906165029-remove-activits.js
@@ -0,0 +1,25 @@
+exports.up = function(db, cb) {
+    const sql = `
+      DROP TABLE activist;
+    `;
+
+    db.runSql(sql, cb);
+};
+
+exports.down = function(db, cb) {
+    const sql = `
+      CREATE TABLE activist (
+        id serial,
+        created_on timestamp with time zone DEFAULT NOW(),
+        updated_on timestamp with time zone DEFAULT NOW(),
+        firstname VARCHAR NOT NULL,
+        lastname VARCHAR NOT NULL,
+        civility VARCHAR NOT NULL;
+        email VARCHAR UNIQUE NOT NULL,
+        phone VARCHAR NOT NULL,
+        PRIMARY KEY (id)
+      );
+    `;
+
+    db.runSql(sql, cb);
+};
