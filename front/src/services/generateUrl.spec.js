affc82078fd8e5b6300931a692b2d13e3c3bdf86
Luwangel <adrien.amoros@gmail.com>
2019-09-09 16:23:50 +0200

    Declare share in generateUrl method

--- front/src/services/generateUrl.spec.js
+++ front/src/services/generateUrl.spec.js
@@ -10,38 +10,43 @@ describe('generateUrl', () => {
     });
 
     it('should generate url for ua/slug', () => {
-        expect(generateUrl('ua', { slug: 'ua-slug-value' })).toBe('/ua/ua-slug-value');
+        expect(generateUrl('ua', { slug: 'ua-slug' })).toBe('/ua/ua-slug');
     });
 
     it('should generate url for ua/slug/story', () => {
-        expect(generateUrl('story', { slug: 'ua-slug-value' })).toBe('/ua/ua-slug-value/story/0');
+        expect(generateUrl('story', { slug: 'ua-slug' })).toBe('/ua/ua-slug/story/0');
     });
 
     it('should generate url for ua/slug/story step', () => {
-        expect(generateUrl('story', { slug: 'ua-slug-value', page: 5 })).toBe(
-            '/ua/ua-slug-value/story/5',
-        );
+        expect(generateUrl('story', { slug: 'ua-slug', page: 5 })).toBe('/ua/ua-slug/story/5');
     });
 
     it('should generate url for ua/slug/act', () => {
-        expect(generateUrl('act', { slug: 'ua-slug-value' })).toBe('/ua/ua-slug-value/act');
+        expect(generateUrl('act', { slug: 'ua-slug' })).toBe('/ua/ua-slug/act');
     });
 
     it('should generate url for ua/slug/message', () => {
-        expect(generateUrl('message', { slug: 'ua-slug-value' })).toBe('/ua/ua-slug-value/message');
+        expect(generateUrl('message', { slug: 'ua-slug' })).toBe('/ua/ua-slug/message');
     });
 
     it('should generate url for ua/slug/thanks', () => {
-        expect(generateUrl('thanks', { slug: 'ua-slug-value' })).toBe('/ua/ua-slug-value/thanks');
+        expect(generateUrl('thanks', { slug: 'ua-slug' })).toBe('/ua/ua-slug/thanks');
+    });
+
+    it('should generate url for ua/slug/register', () => {
+        expect(generateUrl('register', { slug: 'ua-slug' })).toBe('/ua/ua-slug/register');
+    });
+
+    it('should generate url for ua/slug/share', () => {
+        expect(generateUrl('share', { slug: 'ua-slug' })).toBe('/ua/ua-slug/share');
     });
 
     it('should generate url for ua/slug/address', () => {
-        expect(generateUrl('address', { slug: 'ua-slug-value' })).toBe('/ua/ua-slug-value/address');
+        expect(generateUrl('address', { slug: 'ua-slug' })).toBe('/ua/ua-slug/address');
     });
 
     it('should throw error if receiving unknow route', () => {
         expect(() => generateUrl()).toThrow('Unknown route undefined passed to generateUrl');
-
         expect(() => generateUrl('bad_route')).toThrow(
             'Unknown route bad_route passed to generateUrl',
         );
