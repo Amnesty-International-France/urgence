6f38fcb15dbd633de0c1ba6e470f24cf7922b586
Luwangel <adrien.amoros@gmail.com>
2019-09-11 15:12:08 +0200

    Fix addCampaignMember mutation

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -11,7 +11,8 @@ export const addCampaignMember = (urgentActionId, member) =>
     fetch(`${process.env.REACT_APP_API_URL}/graphql`, {
         method: 'POST',
         body: JSON.stringify({
-            addCampaignMemberQuery,
+            operationName: 'AddCampaignMember',
+            query: addCampaignMemberQuery,
             variables: {
                 id: urgentActionId,
                 member,
