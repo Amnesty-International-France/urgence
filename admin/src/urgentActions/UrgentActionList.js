855940358ad544501a49973ee0c7ae681fb49b88
Luwangel <adrien.amoros@gmail.com>
2019-09-12 15:45:31 +0200

    Display is default field in Urgent Action list and edit in admin

--- admin/src/urgentActions/UrgentActionList.js
+++ admin/src/urgentActions/UrgentActionList.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Datagrid, EditButton, List, Responsive, TextField, DateField } from 'react-admin';
+import {
+    Datagrid,
+    EditButton,
+    List,
+    Responsive,
+    TextField,
+    BooleanField,
+    DateField,
+} from 'react-admin';
 
 import dateFormat from '../dateFormat';
 import PreviewLink from './PreviewLink';
@@ -8,19 +16,20 @@ import PreviewLetter from './PreviewLetter';
 const sort = { field: 'last_edition_date', order: 'DESC' };
 
 export default props => (
-    <List {...props} title={'Urgent Actions'} sort={sort} perPage={25}>
+    <List {...props} title="Urgent Actions" sort={sort} perPage={25}>
         <Responsive
             medium={
                 <Datagrid>
                     <TextField source="title" />
-                    <TextField source="campaign_code" label="Campaign code" />
-                    <TextField source="origin_code" label="Origin code" />
+                    <TextField source="campaign_code" label="Campaign Code" />
+                    <TextField source="origin_code" label="Origin Code" />
                     <DateField
                         showTime
                         label="Last Updated"
                         options={dateFormat}
                         source="last_edition_date"
                     />
+                    <BooleanField source="is_default" label="Set As Default" />
                     <EditButton />
                     <PreviewLink />
                     <PreviewLetter />
