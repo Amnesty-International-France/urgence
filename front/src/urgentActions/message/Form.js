7f0be87485442a3f91f2cbb02955b8e0cc83b14a
Luwangel <adrien.amoros@gmail.com>
2019-11-13 15:20:50 +0100

    Create a Form component in message

---
+++ front/src/urgentActions/message/Form.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Input, { isCorrectEmail } from '../../themes/Input';
+import RadioButton from '../../themes/RadioButton';
+
+const Form = ({
+    email,
+    objectIndication,
+    object,
+    civility,
+    firstname,
+    lastname,
+    analyticsCategory,
+    step,
+    setEmail,
+    setObject,
+    setCivility,
+    setFirstname,
+    setLastname,
+}) => {
+    const handleChangeEmail = event => {
+        setEmail(event.target.value);
+    };
+
+    const handleChangeObject = event => {
+        setObject(event.target.value);
+    };
+
+    const handleChangeCivility = event => {
+        setCivility(event.target.value);
+    };
+
+    const handleChangeFirstname = event => {
+        setFirstname(event.target.value);
+    };
+
+    const handleChangeLastname = event => {
+        setLastname(event.target.value);
+    };
+
+    return (
+        <>
+            <Input
+                className="object"
+                value={object}
+                onChange={handleChangeObject}
+                error={!object}
+                analyticsCategory={analyticsCategory}
+                step={step}
+                label="Objet de l'e-mail *"
+            />
+            <p className="objectIndication">{objectIndication}</p>
+            <Input
+                className="email"
+                type="email"
+                value={email}
+                onChange={handleChangeEmail}
+                error={!isCorrectEmail(email)}
+                autoComplete="email"
+                analyticsCategory={analyticsCategory}
+                step={step}
+                label="Votre adresse e-mail *"
+            />
+            <RadioButton
+                value={civility}
+                name="civility"
+                onChange={handleChangeCivility}
+                error={!civility}
+                analyticsCategory={analyticsCategory}
+                step={step}
+                label="Civilité *"
+                autoComplete="civility"
+                choices={['M.', 'Mme.', 'Autre']}
+            />
+            <Input
+                className="firstname"
+                value={firstname}
+                onChange={handleChangeFirstname}
+                error={!firstname}
+                analyticsCategory={analyticsCategory}
+                step={step}
+                autoComplete="given-name"
+                label="Votre prénom *"
+            />
+            <Input
+                className="lastname"
+                value={lastname}
+                onChange={handleChangeLastname}
+                error={!lastname}
+                analyticsCategory={analyticsCategory}
+                step={step}
+                autoComplete="family-name"
+                label="Votre nom *"
+            />
+        </>
+    );
+};
+
+Form.propTypes = {
+    className: PropTypes.string,
+    email: PropTypes.string,
+    objectIndication: PropTypes.string,
+    object: PropTypes.string,
+    civility: PropTypes.string,
+    firstname: PropTypes.string,
+    lastname: PropTypes.string,
+    setEmail: PropTypes.func.isRequired,
+    setObject: PropTypes.func.isRequired,
+    setCivility: PropTypes.func.isRequired,
+    setFirstname: PropTypes.func.isRequired,
+    setLastname: PropTypes.func.isRequired,
+    analyticsCategory: PropTypes.string,
+    step: PropTypes.string,
+};
+
+export default Form;
---
+++ front/src/urgentActions/message/Form.spec.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Input from '../../themes/Input';
+import RadioButton from '../../themes/RadioButton';
+
+import Form from './Form';
+
+describe('Form', () => {
+    const defaultStep = [{ value: 'one' }, { value: 'two' }, { value: 'three' }];
+    const defaultProps = {
+        messageTemplate: defaultStep,
+        objectIndication: 'object indication',
+        loading: false,
+        match: { params: {} },
+        history: { push: () => null },
+        recipient: {
+            mail: 'mail',
+        },
+    };
+
+    it('display all inputs', () => {
+        const wrapper = shallow(<Form {...defaultProps} />);
+
+        const inputs = wrapper.find(Input);
+
+        expect(inputs.at(0).prop('className')).toBe('object');
+        expect(inputs.at(0).prop('label')).toBe(`Objet de l'e-mail *`);
+
+        expect(inputs.at(1).prop('className')).toBe('email');
+        expect(inputs.at(1).prop('label')).toBe(`Votre adresse e-mail *`);
+
+        expect(inputs.at(2).prop('className')).toBe('firstname');
+        expect(inputs.at(2).prop('label')).toBe(`Votre prénom *`);
+
+        expect(inputs.at(3).prop('className')).toBe('lastname');
+        expect(inputs.at(3).prop('label')).toBe(`Votre nom *`);
+    });
+
+    it('display a radio button', () => {
+        const wrapper = shallow(<Form {...defaultProps} />);
+
+        const radio = wrapper.find(RadioButton);
+
+        expect(radio.at(0).prop('label')).toBe('Civilité *');
+    });
+});
