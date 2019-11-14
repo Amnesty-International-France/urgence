cfb873546a777809a6293837514191a7cc433012
Luwangel <adrien.amoros@gmail.com>
2019-11-14 15:49:03 +0100

    Improve the MessageInput in admin

--- admin/src/urgentActions/MessageInput.js
+++ admin/src/urgentActions/MessageInput.js
@@ -47,85 +47,86 @@ export const validateEmailsList = text =>
         ? 'Must contain only emails separated by a comma.'
         : null;
 
-const initMessageTemplate = [{ value: '' }];
-
 export const MessageInput = ({ classes, source }) => (
     <div className={classNames(classes.root, { [classes.bordered]: LETTER_ACTIVATED })}>
         <FormDataConsumer>
-            {({ formData }) => (
-                <Fragment>
-                    <Avatar className={classes.avatar}>{getScreenIndex(MESSAGE, formData)}</Avatar>
-                    <Card className={classes.card}>
-                        <CardContent className={classes.content}>
-                            <div className={classes.formContainer}>
-                                <RichTextInput
-                                    fullWidth
-                                    multiline
-                                    label="Message Tip"
-                                    source={`${source}.text`}
-                                    defaultValue="Parce que les messages uniques ont plus d'impact nous vous invitons à personnaliser l'objet de l'email."
-                                />
-                                <RichTextInput
-                                    fullWidth
-                                    multiline
-                                    label="Object Tip"
-                                    source={`${source}.object_indication`}
-                                    defaultValue="Indiquez par exemple que vous souhaitez parler de cette situation inacceptable."
-                                />
-                                <Labeled label="Recipient">
-                                    <Fragment>
-                                        <TextInput
-                                            fullWidth
-                                            type="email"
-                                            label="Mail To"
-                                            source={`${source}.recipient.mail`}
-                                            validate={validateRecipientEmail}
-                                        />
-                                        <TextInput
-                                            fullWidth
-                                            label="Copies To"
-                                            source={`${source}.recipient.copies_to`}
-                                            validate={validateEmailsList}
-                                        />
-                                        <TextInput
-                                            fullWidth
-                                            label="CCI"
-                                            source={`${source}.recipient.cci`}
-                                            validate={validateEmailsList}
-                                        />
-                                    </Fragment>
-                                </Labeled>
-                                <ArrayInput label="Message" source={`${source}.message_template`}>
-                                    <SimpleParagraphFormIterator>
-                                        <ParagraphTemplateInput source="" />
-                                    </SimpleParagraphFormIterator>
-                                </ArrayInput>
-                            </div>
-                        </CardContent>
-                    </Card>
-                    {formData[source].message_template && (
+            {({ formData }) => {
+                const data = formData[source];
+
+                return (
+                    <Fragment>
+                        <Avatar className={classes.avatar}>
+                            {getScreenIndex(MESSAGE, formData)}
+                        </Avatar>
+                        <Card className={classes.card}>
+                            <CardContent className={classes.content}>
+                                <div className={classes.formContainer}>
+                                    <RichTextInput
+                                        fullWidth
+                                        multiline
+                                        label="Message Tip"
+                                        source={`${source}.text`}
+                                        defaultValue="Parce que les messages uniques ont plus d'impact nous vous invitons à personnaliser l'objet de l'email."
+                                    />
+                                    <RichTextInput
+                                        fullWidth
+                                        multiline
+                                        label="Object Tip"
+                                        source={`${source}.object_indication`}
+                                        defaultValue="Indiquez par exemple que vous souhaitez parler de cette situation inacceptable."
+                                    />
+                                    <Labeled label="Recipient">
+                                        <Fragment>
+                                            <TextInput
+                                                fullWidth
+                                                type="email"
+                                                label="Mail To"
+                                                source={`${source}.recipient.mail`}
+                                                validate={validateRecipientEmail}
+                                            />
+                                            <TextInput
+                                                fullWidth
+                                                label="Copies To"
+                                                source={`${source}.recipient.copies_to`}
+                                                validate={validateEmailsList}
+                                            />
+                                            <TextInput
+                                                fullWidth
+                                                label="CCI"
+                                                source={`${source}.recipient.cci`}
+                                                validate={validateEmailsList}
+                                            />
+                                        </Fragment>
+                                    </Labeled>
+                                    <ArrayInput
+                                        label="Message"
+                                        source={`${source}.message_template`}
+                                    >
+                                        <SimpleParagraphFormIterator>
+                                            <ParagraphTemplateInput source="" />
+                                        </SimpleParagraphFormIterator>
+                                    </ArrayInput>
+                                </div>
+                            </CardContent>
+                        </Card>
                         <FrontPreview className={classes.preview}>
-                            <Message
-                                text={formData[source].text || ''}
-                                messageTemplate={
-                                    formData[source].message_template &&
-                                    formData[source].message_template[0] &&
-                                    formData[source].message_template[0].value
-                                        ? formData[source].message_template
-                                        : initMessageTemplate
-                                }
-                                objectIndication={formData[source].object_indication || ''}
-                                action={<Link to="#" label="J'envoie" />}
-                                setEmail={() => {}}
-                                setObject={() => {}}
-                                setCivility={() => {}}
-                                setFirstname={() => {}}
-                                setLastname={() => {}}
-                            />
+                            {data && (
+                                <Message
+                                    text={data.text || ''}
+                                    messageTemplate={data.message_template}
+                                    objectIndication={data.object_indication || ''}
+                                    action={<Link to="#" label="J'envoie" />}
+                                    setEmail={() => {}}
+                                    setObject={() => {}}
+                                    setCivility={() => {}}
+                                    setFirstname={() => {}}
+                                    setLastname={() => {}}
+                                />
+                            )}
                         </FrontPreview>
-                    )}
-                </Fragment>
-            )}
+                    </Fragment>
+                );
+            }}
         </FormDataConsumer>
     </div>
 );
