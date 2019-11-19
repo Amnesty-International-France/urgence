1115c7e0df707de01f1f404f151655dc59ff4c31
Luwangel <adrien.amoros@gmail.com>
2019-11-19 15:39:49 +0100

    Remove unused colors in Alert

--- front/src/themes/Alert.js
+++ front/src/themes/Alert.js
@@ -7,12 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import MobileDetect from 'mobile-detect';
 
-import { white } from '../themes/colors';
 import { RichText } from './RichText';
-
-const alertStyles = {
-    color: `${white} !important`,
-};
 
 const styles = {
     icon: {
@@ -39,7 +34,7 @@ const Alert = ({ classes, message }) => {
             ContentProps={{
                 'aria-describedby': 'message-id',
             }}
-            message={<RichText html={message} style={alertStyles} />}
+            message={<RichText html={message} />}
             TransitionComponent={props => <Slide {...props} direction="up" />}
             action={
                 <FontAwesomeIcon
