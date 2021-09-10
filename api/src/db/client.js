1eb38295e790b0b600e3bce292640cf5fa874e23
Luwangel <adrien.amoros@gmail.com>
2021-09-10 13:56:43 +0200

    Update the sql client to allow params

--- api/src/db/client.js
+++ api/src/db/client.js
@@ -4,12 +4,13 @@ const config = require('../../../config');
 
 const pool = new PgPool.default(config.db);
 
-module.exports = async query => {
+module.exports = async (query, param) => {
     const client = await pool.connect();
 
     try {
-        const response = client.query(query);
+        const response = client.query(query, param);
         client.release();
+
         return response;
     } catch (err) {
         client.end();
