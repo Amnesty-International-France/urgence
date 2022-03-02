311642116ae5b78c37caf13ad633492cf8b6c7db
Guillaume BILLEY <guilbill@gmail.com>
2022-03-02 15:16:25 +0100

    Remove console.log

--- api/src/services/uploadImage.js
+++ api/src/services/uploadImage.js
@@ -20,13 +20,9 @@ export const uploadImage = async (upload, crop) => {
     const lastUrlParam = /\/([^\/]*$)/;
     if (typeof upload === 'string') {
         url = upload;
-        console.log({ url });
-        console.log(upload.match(lastUrlParam)[1]);
         const filename = upload.match(lastUrlParam)[1];
-        console.log({ filename });
         path = `${config.uploadDir}/${filename}`;
         cropPath = `${config.uploadDir}/crop-${filename}`;
-        console.log(`${path} ${cropPath}`);
     } else {
         const rawFile = await upload.rawFile;
         if (!rawFile.stream) {
