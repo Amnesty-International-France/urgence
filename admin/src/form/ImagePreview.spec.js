6a1ea903a5aefe2cadce13f70fe02ced3a8c4983
Guillaume BILLEY <guilbill@gmail.com>
2022-03-03 10:59:18 +0100

    fix-crop

--- admin/src/form/ImagePreview.spec.js
+++ admin/src/form/ImagePreview.spec.js
@@ -5,6 +5,9 @@ import { ImageField } from 'react-admin';
 import { ImagePreview } from './ImagePreview';
 
 describe('ImagePreview', () => {
+    beforeEach(() => {
+        jest.spyOn(React, 'useState').mockImplementation(() => [{}, () => {}]);
+    });
     it('should render ImageField with record props from props.record', () => {
         const wrapper = shallow(
             <ImagePreview record="imageUrl" input={{ value: undefined, onChange: undefined }} />,
