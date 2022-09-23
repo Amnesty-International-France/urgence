5f8ec8ec998223a64e85705e2313fe52a5f8ddb1
Florian Ferbach <florian.ferbach@univ-lorraine.fr>
2022-09-23 11:06:45 +0200

    Add a wait to remove the crop if it doesn't go well

--- api/src/services/uploadImageDesktopFromStory.ts
+++ api/src/services/uploadImageDesktopFromStory.ts
@@ -12,9 +12,19 @@ export const uploadImageDesktopFromStory = async (story: UrgentAction['story'] =
     });
 
     const srcs = await Promise.all(
-        images.map((img) => uploadImage(img.src as ImageUpload, img.crop)),
+        images.map(async (image) => {
+            try {
+                return await uploadImage(image.src as ImageUpload, image.crop);
+            } catch (error: any) {
+                if (error.message.includes('extract_area: bad extract area')) {
+                    image.crop = undefined;
+                    return await uploadImage(image.src as ImageUpload, undefined);
+                }
+                throw error;
+            }
+        }),
     );
-    
+
     return story.map((storyStep, index) =>
         srcs[index] ? assocPath(['mediumDesktop', 'src'], srcs[index], storyStep) : storyStep,
     );
