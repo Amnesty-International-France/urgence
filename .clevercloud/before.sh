a86d74ce43acee354d662f9c6fdb927a5e48dc1d
Alexis Lefebvre <alexis@troopers.email>
2022-11-28 17:21:30 +0100

    WIP: debug node and yarn

--- .clevercloud/before.sh
+++ .clevercloud/before.sh
@@ -1,7 +1,18 @@
+echo "node version"
+echo "-----"
+node --version
+echo "-----"
+
 echo "yarn version"
 echo "-----"
 yarn --version
 echo "-----"
+
+echo "whereis yarn"
+echo "-----"
+whereis yarn
+echo "-----"
+
 yarn install
 #yarn run build -vvv
 #yarn workspaces focus --production reaction-rapide-api -vvv
