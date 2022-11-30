8bc248250e46dc5a69cb93bbdc08447b3407df92
Alexis Lefebvre <alexis@troopers.email>
2022-11-30 13:54:52 +0100

    Revert "deploy: run yarn workspaces …"
    This reverts commit aa6b9d0b28be82440590c7092624d593a39d6c0f.

--- .clevercloud/after.sh
+++ .clevercloud/after.sh
@@ -1,3 +1,2 @@
-yarn workspaces focus --production reaction-rapide-api
-cd api/
-npx db-migrate --config=database.cjs --migrations-dir=migrations --env api up
+#cd api/
+#npx db-migrate --config=database.cjs --migrations-dir=migrations --env api up
