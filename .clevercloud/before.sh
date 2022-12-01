e344d30bc422df1b012746f039c02370bd5fa267
Alexis Lefebvre <alexis@troopers.email>
2022-12-01 15:34:05 +0100

    deploy static content on a apache instance

--- .clevercloud/before.sh
+++ .clevercloud/before.sh
@@ -22,36 +22,6 @@ echo "-----"
 whereis yarn config get cacheFolder
 echo "-----"
 
-echo "build amnesty-components/"
-echo "-----"
-cd amnesty-components/
-yarn install
-yarn run build
-cd ..
-echo "-----"
-echo "amnesty-components/ has been built"
-echo "-----"
-
-echo "build admin/"
-echo "-----"
-cd admin/
-yarn install
-yarn run build
-cd ..
-echo "-----"
-echo "admin/ has been built"
-echo "-----"
-
-echo "build front/"
-echo "-----"
-cd front/
-yarn install
-yarn run build-web
-cd ..
-echo "-----"
-echo "front/ has been built"
-echo "-----"
-
 echo "build api/"
 echo "-----"
 cd api/
@@ -65,5 +35,5 @@ echo "-----"
 #yarn workspaces focus --production reaction-rapide-api -vvv
 
 echo "-----"
-echo "after.sh has been executed"
+echo "before.sh has been executed"
 echo "-----"
