77d84629143046cc48d141ab1476a84096e8540d
Alexis Lefebvre <alexis@troopers.email>
2022-12-01 19:48:25 +0100

    front/ and admin/ need different values for PUBLIC_URL

--- .clevercloud/static/build.sh
+++ .clevercloud/static/build.sh
@@ -8,12 +8,14 @@ cd ..
 
 echo "build admin/"
 cd admin/
+export PUBLIC_URL=${ADMIN_PUBLIC_URL}
 node ../.yarn/releases/yarn-3.2.1.cjs install
 node ../.yarn/releases/yarn-3.2.1.cjs run build
 cd ..
 
 echo "build front/"
 cd front/
+export PUBLIC_URL=${FRONT_PUBLIC_URL}
 node ../.yarn/releases/yarn-3.2.1.cjs install
 node ../.yarn/releases/yarn-3.2.1.cjs run build-web
 cd ..
