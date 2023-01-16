d6098bef72fa1b8a039271b5fd2678cab49b52ac
Alexis Lefebvre <alexis@troopers.email>
2023-01-16 12:54:43 +0100

    use yarn workspaces on Clever Cloud

--- .clevercloud/run.sh
+++ .clevercloud/run.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+# Print commands and their arguments as they are executed
+set -xe
+
 echo "-----"
 echo "execute run.sh"
 echo "-----"
--- .clevercloud/static/build.sh
+++ .clevercloud/static/build.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+# Print commands and their arguments as they are executed
+set -xe
+
+yarn workspaces focus amnesty-components
+yarn workspaces focus urgent_reaction
+yarn workspaces focus reaction-rapide-admin
+
 echo "build amnesty-components/"
 cd amnesty-components/
-node ../.yarn/releases/yarn-3.2.1.cjs install
-node ../.yarn/releases/yarn-3.2.1.cjs run build
+yarn run build
 cd ..
 
 echo "build admin/"
 cd admin/
 export PUBLIC_URL=${ADMIN_PUBLIC_URL}
-node ../.yarn/releases/yarn-3.2.1.cjs install
-node ../.yarn/releases/yarn-3.2.1.cjs run build
+yarn run build
 cd ..
 
 echo "build front/"
 cd front/
 export PUBLIC_URL=${FRONT_PUBLIC_URL}
-node ../.yarn/releases/yarn-3.2.1.cjs install
-node ../.yarn/releases/yarn-3.2.1.cjs run build-web
+yarn run build-web
 cd ..
