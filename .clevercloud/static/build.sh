30ee53d1663544b6fc7ef11fc6443a82685fabf7
Alexis Lefebvre <alexis@troopers.email>
2023-01-16 15:11:24 +0100

    don't use "yarn workspaces" when building the front on CI

--- .clevercloud/static/build.sh
+++ .clevercloud/static/build.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
-# Print commands and their arguments as they are executed
-set -xe
-
-yarn workspaces focus amnesty-components
-yarn workspaces focus urgent_reaction
-yarn workspaces focus reaction-rapide-admin
-
 echo "build amnesty-components/"
 cd amnesty-components/
-yarn run build
+node ../.yarn/releases/yarn-3.2.1.cjs install
+node ../.yarn/releases/yarn-3.2.1.cjs run build
 cd ..
 
 echo "build admin/"
 cd admin/
 export PUBLIC_URL=${ADMIN_PUBLIC_URL}
-yarn run build
+node ../.yarn/releases/yarn-3.2.1.cjs install
+node ../.yarn/releases/yarn-3.2.1.cjs run build
 cd ..
 
 echo "build front/"
 cd front/
 export PUBLIC_URL=${FRONT_PUBLIC_URL}
-yarn run build-web
+node ../.yarn/releases/yarn-3.2.1.cjs install
+node ../.yarn/releases/yarn-3.2.1.cjs run build-web
 cd ..
