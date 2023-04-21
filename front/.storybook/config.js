84d51d187ed86bad7030604d616e94a511a4b08e
Jean-Baptiste Oellers <jean-baptiste.oellers@incaya.fr>
2023-04-21 11:03:48 +0200

    chore: remove storybook and unused deploy script

--- front/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-    require('../src/icons/stories');
-    require('../src/themes/stories');
-    require('../src/urgentActions/stories');
-}
-
-configure(loadStories, module);
--- front/.storybook/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = (baseConfig, env, defaultConfig) => {
-    defaultConfig.output.publicPath = '';
-
-    return defaultConfig;
-};
