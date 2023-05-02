74fce146f0425fb46c32e578a37b26c4587b047a
Jean-Baptiste Oellers <jean-baptiste.oellers@incaya.fr>
2023-05-02 14:39:08 +0200

    chore: update ra-data-graphql-simple

--- admin/src/dataProvider.ts
+++ admin/src/dataProvider.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { createUploadLink } from 'apollo-upload-client';
 import buildApolloClient from 'ra-data-graphql-simple';
@@ -18,8 +18,23 @@ const authLink = setContext((_, { headers }) => {
     };
 });
 
+// https://stackoverflow.com/questions/52786220/how-to-fix-graphql-mutations-typename-errors
+const omitTypename = (key: String, value: String) => {
+    return key === '__typename' ? undefined : value
+}
+
+const omitTypenameLink = new ApolloLink((operation, forward) => {
+    if (operation.variables) {
+        operation.variables = JSON.parse(
+            JSON.stringify(operation.variables),
+            omitTypename
+        )
+    }
+    return forward(operation)
+})
+
 export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link:  ApolloLink.from([authLink, omitTypenameLink, httpLink]),
     cache: new InMemoryCache(),
 });
 
