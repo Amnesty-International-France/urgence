bb3a010bf61df820fe2a29a599f0e96905cbbb85
Alexis Janvier <alexis.janvier@incaya.fr>
2023-06-05 16:23:07 +0200

    fix: test du lien mailto en admin

--- admin/src/urgentActions/MailtoCheck.tsx
+++ admin/src/urgentActions/MailtoCheck.tsx
@@ -8,7 +8,7 @@ export const MailtoCheck = ({ mailto }: MailtoCheckProps) => {
     const [status, setStatus] = useState('en attente d\'un clic');
 
     const checkFocus = () => {
-        if (navigator.userAgent.indexOf('Win') && document.hasFocus()) {
+        if (navigator.userAgent.indexOf('Win') !== -1 && document.hasFocus()) {
             setStatus('❌ l\'ouverture du lien mailto n\'a pas fonctionné');
         } else {
             setStatus('✅ l\'ouverture du lien mailto a fonctionné');
