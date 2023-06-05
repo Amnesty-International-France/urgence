be513174962ef5ae565e9fd61d331bab460e0a3d
Alexis Janvier <alexis.janvier@incaya.fr>
2023-06-05 16:39:39 +0200

    chore: refacto mineur

--- admin/src/urgentActions/form/Part5MessageView.tsx
+++ admin/src/urgentActions/form/Part5MessageView.tsx
@@ -1,116 +1,13 @@
 import Box from '@mui/material/Box';
 import { red } from '@mui/material/colors';
 
-import { FormDataConsumer } from 'react-admin';
-
 import { MessageViewInput } from '../MessageViewInput';
-
-export type Medium = {
-    title: string;
-    src: string;
-    crop?: {
-        x: number;
-        y: number;
-        width: number;
-        height: number;
-        unit: string;
-    };
-};
-export type StoryStep = {
-    content: string;
-    medium?: Medium;
-    mediumDesktop?: Medium;
-};
-
-export type TwitterAction = {
-    title: string;
-    message: string;
-    hashtags: string;
-    url: string;
-};
-
-export type CallToAction = {
-    title: string;
-    message: string;
-    button: string;
-    interpelation_mode: 'email' | 'rs';
-    progress: {
-        display: boolean;
-        message: string;
-        display_threshold: number | null;
-        objective: number | null;
-    };
-    twitter_action?: TwitterAction;
-};
-
-export type FormData = {
-    id: string;
-    title: string;
-    slug: string;
-    is_default: boolean;
-    campaign_code: string;
-    origin_code: string;
-    story?: StoryStep[];
-    call_to_action?: CallToAction;
-    creation_date: string;
-    last_edition_date: string;
-    response_count: number;
-    social_metadata?: {
-        title: string;
-        description: string;
-        medium?: Medium;
-    };
-    email_thank?: {
-        title: string;
-        text: string;
-        share: {
-            twitter_message: string;
-            message: string;
-        };
-        telegram: {
-            message: string;
-            url: string;
-        };
-    };
-    end_thank?: {
-        title: string;
-        text: string;
-    };
-    message?: {
-        text_view: string;
-        text_send: string;
-        button_view: string;
-        button_send: string;
-        object_example: string;
-        object_indication: string;
-        message_template: { value: string }[];
-        recipient: {
-            mail: string;
-            copies_to: string;
-            cci: string;
-        };
-    };
-    register?: {
-        button: string;
-        phone_indication: string;
-        text: string;
-        title: string;
-    };
-};
 
 const Part5MessageView = () => {
     return (
-        <FormDataConsumer>
-            {({ formData }: { formData: FormData }) => {
-                if (formData.call_to_action?.interpelation_mode === 'email') {
-                    return (
-                            <Box sx={{ backgroundColor: red[50] }}>
-                                <MessageViewInput source="message" />
-                            </Box>
-                    );
-                }
-            }}
-        </FormDataConsumer>
+        <Box sx={{ backgroundColor: red[50] }}>
+            <MessageViewInput source="message" />
+        </Box>
     );
 };
 
--- admin/src/urgentActions/form/index.tsx
+++ admin/src/urgentActions/form/index.tsx
@@ -1,3 +1,5 @@
+import { FormDataConsumer } from 'react-admin';
+
 import { LongForm } from './LongForm';
 import Part1Camgaign from './Part1Campaign';
 import Part2SocialNetworks from './Part2SocialNetworks';
@@ -8,6 +10,99 @@ import Part6MessageSend from './Part6MessageSend';
 import Part7Share from './Part7Share';
 import Part8Register from './Part8Register';
 import Part9Thanks from './Part9Thanks';
+
+export type Medium = {
+    title: string;
+    src: string;
+    crop?: {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+        unit: string;
+    };
+};
+export type StoryStep = {
+    content: string;
+    medium?: Medium;
+    mediumDesktop?: Medium;
+};
+
+export type TwitterAction = {
+    title: string;
+    message: string;
+    hashtags: string;
+    url: string;
+};
+
+export type CallToAction = {
+    title: string;
+    message: string;
+    button: string;
+    interpelation_mode: 'email' | 'rs';
+    progress: {
+        display: boolean;
+        message: string;
+        display_threshold: number | null;
+        objective: number | null;
+    };
+    twitter_action?: TwitterAction;
+};
+
+export type FormData = {
+    id: string;
+    title: string;
+    slug: string;
+    is_default: boolean;
+    campaign_code: string;
+    origin_code: string;
+    story?: StoryStep[];
+    call_to_action?: CallToAction;
+    creation_date: string;
+    last_edition_date: string;
+    response_count: number;
+    social_metadata?: {
+        title: string;
+        description: string;
+        medium?: Medium;
+    };
+    email_thank?: {
+        title: string;
+        text: string;
+        share: {
+            twitter_message: string;
+            message: string;
+        };
+        telegram: {
+            message: string;
+            url: string;
+        };
+    };
+    end_thank?: {
+        title: string;
+        text: string;
+    };
+    message?: {
+        text_view: string;
+        text_send: string;
+        button_view: string;
+        button_send: string;
+        object_example: string;
+        object_indication: string;
+        message_template: { value: string }[];
+        recipient: {
+            mail: string;
+            copies_to: string;
+            cci: string;
+        };
+    };
+    register?: {
+        button: string;
+        phone_indication: string;
+        text: string;
+        title: string;
+    };
+};
 
 const ActionForm = () => {
     return (
@@ -24,12 +119,22 @@ const ActionForm = () => {
             <LongForm.Section label="Call to action">
                 <Part4CallToAction />
             </LongForm.Section>
-            <LongForm.Section label="Message view">
-                <Part5MessageView />
-            </LongForm.Section>
-            <LongForm.Section label="Message send">
-                <Part6MessageSend />
-            </LongForm.Section>
+            <FormDataConsumer>
+                {({ formData }: { formData: FormData }) => {
+                    if (formData.call_to_action?.interpelation_mode === 'email') {
+                        return (
+                            <>
+                                <LongForm.Section label="Message view">
+                                    <Part5MessageView />
+                                </LongForm.Section>
+                                <LongForm.Section label="Message send">
+                                    <Part6MessageSend />
+                                </LongForm.Section>
+                            </>
+                        );
+                    }
+                }}
+            </FormDataConsumer>
             <LongForm.Section label="Share">
                 <Part7Share />
             </LongForm.Section>
